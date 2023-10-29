eb7cbc47e5a45b8e651e82df36021a37f6381f40
83CarloG <cgcolombo1983@gmail.com>
2023-10-29 19:23:38 +0100

    add Services(import/export/event/async/await)

---
+++ app.js
@@ -0,0 +1,20 @@
+import Store from './services/Store.js';
+import API from './services/API.js';
+import { loadData } from './services/Menu.js';
+
+window.app = {}
+app.store = Store;
+
+// It's better to wait for the event for manipulation
+window.addEventListener('DOMContentLoaded', () => {
+    loadData();
+    console.log(app.store)
+})
+
+const $ = function(args){ return document.querySelector(args);}
+const $$ = function(args){ return document.querySelectorAll(args);}
+
+HTMLElement.prototype.on = function(a, b, c){ return this.addEventListener(a, b, c); }
+HTMLElement.prototype.off = function(a, b){ return this.removeEventListener(a, b); }
+HTMLElement.prototype.$ = function(s){ return this.querySelector(s); }
+HTMLElement.prototype.$$ = function(s){ return this.querySelectorAll(s); }
